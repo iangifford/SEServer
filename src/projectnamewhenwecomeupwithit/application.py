588aad3f50e43b56bdf087d1fac17e2279829d03
Ian Gifford <ian@thegiffordteam.org>
2020-11-08 16:53:35 -0500

    flask basic setup

--- src/projectnamewhenwecomeupwithit/application.py
+++ src/projectnamewhenwecomeupwithit/application.py
@@ -1,1 +1,6 @@
-#flask startup point
+from flask import Flask, send_from_directory, request
+app  = Flask(__name__, static_url_path='')
+
+@app.route('/<path:path>')
+def static_files(path=None):
+    return send_from_directory('static',path)
