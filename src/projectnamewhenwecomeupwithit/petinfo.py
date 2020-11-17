6da1687e062d98e17093039da31a3304b32e1f13
Ian Gifford <ian@thegiffordteam.org>
2020-11-16 21:24:16 -0500

    example data sending

--- src/projectnamewhenwecomeupwithit/petinfo.py
+++ src/projectnamewhenwecomeupwithit/petinfo.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restful import Resource, Api
 
 app_api = None
@@ -9,6 +9,6 @@ def create_api(app):
 
 class PetInfoEndpoint(Resource):
     def get(self,data):
-        return {"Get":data}
+        return send_from_directory('static','/test_data/petinfoendpoint.json')
     def post(self,data):
         return {"Post":data}
