27b2509a81c3242bb5b95fad96ffed4a9ffeb3ea
Ian Gifford <ian@thegiffordteam.org>
2020-12-02 17:54:43 -0500

    verifyauth

--- src/petsittingco/resources/pet.py
+++ src/petsittingco/resources/pet.py
@@ -1,19 +1,36 @@
 from flask import Flask, send_from_directory
-from flask_restful import Resource, Api
-
-
+from flask_restful import Resource, Api, reqparse
+from src.petsittingco.database import db, Pet, Account, Job
+from src.petsittingco.resources.verify_auth import verify_auth
 app_api = None
 
 def create_api(app):
     app_api = Api(app)
     app_api.add_resource(PetInfo,"/petinfo")
-    app_api.add_resource(PetCreation,"/petcreation/<string:data>")
+    app_api.add_resource(PetCreation,"/petcreation")
+    #delete pet
+    #modify pet
+
     
+    
+
 class PetInfo(Resource):
     def get(self):
-        return send_from_directory('test_data','petinfoendpoint.json')
-    def post(self):
-        return {"Post":"data"}
+        parser = reqparse.RequestParser()
+        parser.add_argument('id',type=str)
+        parser.add_argument('auth',type=str)
+        #parser add argument for pet id
+        args = parser.parse_args()
+        if(verify_auth(args["auth"],args["id"])):
+            #make sure the pet belongs to the owner
+
+            acc = Account.query.get(str(args["id"]))
+            pet = Pet.query.get(asdwadaw)
+            if pet.owner == args["id"]
+                return json ---> , 200
+            return send_from_directory('test_data','petinfoendpoint.json') #{"pet_name":pet.name, "attributes":pet.atributes}
+        return "bad pet", 404
+
 
 class PetCreation(Resource):
     def get(self,data):
--- src/petsittingco/test_data/test_file.py
+++ src/petsittingco/test_data/test_file.py
@@ -1,5 +1,5 @@
 import requests
 
-r = requests.get("http://petinfo.json")
+r = requests.get("http://petinfo.json")#/petinfo?id=1&pet_id=1&auth=1
 r.status_code
 r.json
