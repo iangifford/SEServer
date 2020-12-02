3a56a15a4674668719529e73074c3cc6911335e2
Ian Gifford <ian@thegiffordteam.org>
2020-12-01 22:51:18 -0500

    uuid to str on return acc create

--- src/petsittingco/resources/account.py
+++ src/petsittingco/resources/account.py
@@ -57,9 +57,9 @@ class AccountCreate(Resource):
             acc = Account(id=str(created_id),is_owner = args["is_owner"], is_sitter = args["is_sitter"], is_admin = args["is_admin"], is_shelter = args["is_shelter"], first_name = args["first_name"], last_name = args["last_name"], email = args["email"], password = args["password"])
             db.session.add(acc)
             db.session.commit()
-            return {"id":created_id}, 201 
+            return {"id":str(created_id)}, 201 
         except Exception  as e:
             print(e)
-            return {"msg":"Bad account parameters."}, 200
+            return {"msg":"Bad account parameters."}, 400
 
 
