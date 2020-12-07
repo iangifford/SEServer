43d87f8ce934099ea5741e7984b8a7758f261e4a
Ian Gifford <ian@thegiffordteam.org>
2020-12-07 17:38:04 -0500

    sitterjoblist and available job list

--- src/petsittingco/resources/job.py
+++ src/petsittingco/resources/job.py
@@ -10,6 +10,8 @@ def create_api(app):
     app_api.add_resource(JobCreation,"/jobcreation")
     app_api.add_resource(JobInfo,"/jobinfo")
     app_api.add_resource(OwnerJobList,"/ownerjoblist")
+    app_api.add_resource(SitterJobList,"/sitterjoblist")
+    app_api.add_resource(AvailableJobList,"/availablejoblist")
 class JobCreation(Resource):
     def post(self):
         parser = reqparse.RequestParser() 
@@ -112,6 +114,48 @@ class OwnerJobList(Resource):
                     sitter_name = "No Sitter"
                 if job.accepted == args["is_accepted"]:
                     job_dict[job.id] = {"sitter_name":sitter_name, "start_datetime":job.start_datetime, "end_datetime":job.end_datetime}
+            job_dict["success"] = True
+            print("job_dict:",job_dict)
+            return job_dict, 200 
+        return {"success":False},404
+
+class SitterJobList(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id',type=str)
+        parser.add_argument('auth', type=str)
+        parser.add_argument('is_canceled', type=bool)
+        args = parser.parse_args()
+        if verify_auth(args['auth'],args['id']):
+            acc = Account.query.get( str(args["id"]) )
+            if not acc:
+                return {"msg":"No Account."}, 400
+            job_array = acc.sitter_jobs
+            job_dict = {}
+            for job in job_array:
+                owner = Job.query.get(job.owner_id)
+                owner_name = owner.first_name
+                if job.canceled == args["is_canceled"]:
+                    job_dict[job.id] = {"owner_name":owner_name, "start_datetime":job.start_datetime, "end_datetime":job.end_datetime}
+            job_dict["success"] = True
+            print("job_dict:",job_dict)
+            return job_dict, 200 
+        return {"success":False},404
+
+class AvailableJobList(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id',type=str)
+        parser.add_argument('auth', type=str)
+        args = parser.parse_args()
+        if verify_auth(args['auth'],args['id']):
+            job_array = Job.query.all()
+            job_dict = {}
+            for job in job_array:
+                owner = Job.query.get(job.owner_id)
+                owner_name = owner.first_name
+                if not (job.accepted or job.canceled):
+                    job_dict[job.id] = {"location":job.location,"owner_name":owner_name,"pet_id":job.pet_id, "start_datetime":job.start_datetime, "end_datetime":job.end_datetime}
             job_dict["success"] = True
             print("job_dict:",job_dict)
             return job_dict, 200 
