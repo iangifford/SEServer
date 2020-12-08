bf67dc1b124bd51c67fd23b263a37cf136f40ad7
Ian Gifford <ian@thegiffordteam.org>
2020-12-07 19:30:36 -0500

    debug ownerjoblist

--- src/petsittingco/resources/job.py
+++ src/petsittingco/resources/job.py
@@ -107,6 +107,7 @@ class OwnerJobList(Resource):
             job_array = acc.owner_jobs
             job_dict = {}
             for job in job_array:
+                print(job)
                 sitter = Job.query.get(job.sitter_id)
                 sitter_name = ""
                 if sitter:
