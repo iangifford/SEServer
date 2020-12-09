aea1b1a0692ccdf5b072838a01cde68c62fab62a
Isabelle <imrivera@loyola.edu>
2020-12-09 01:02:15 -0500

    Fixed html for the button links, previous error did not go to actual dashboard

--- src/petsittingco/main_dash.py
+++ src/petsittingco/main_dash.py
@@ -11,16 +11,16 @@ def main_dashboard():
     all_buttons = ""
     # Owner
     if current_user.is_owner:
-        all_buttons += '<a href="ownerdashboard/dashboard.html" class="custom-btn btn-bg btn mt-3" data-aos-delay="100">Owner Dashboard</a><br>'
+        all_buttons += '<a href="../ownerdashboard/dashboard.html" class="custom-btn btn-bg btn mt-3" data-aos-delay="100">Owner Dashboard</a><br>'
     # Sitter
     if current_user.is_sitter:
-        all_buttons += '<a href="sitterdashboard/dashboard.html" class="custom-btn btn-bg btn mt-3" data-aos-delay="100">Sitter Dashboard</a><br>'
+        all_buttons += '<a href="../sitterdashboard/dashboard.html" class="custom-btn btn-bg btn mt-3" data-aos-delay="100">Sitter Dashboard</a><br>'
     # Shelter
     if current_user.is_shelter:
-        all_buttons += '<a href="shelterdashboard/dashboard.html" class="custom-btn btn-bg btn mt-3" data-aos-delay="100">Shelter Dashboard</a><br>'
+        all_buttons += '<a href="../shelterdashboard/dashboard.html" class="custom-btn btn-bg btn mt-3" data-aos-delay="100">Shelter Dashboard</a><br>'
     #Admin
     if current_user.is_admin:
-        all_buttons += '<a href="admindashboard/dashboard.html" class="custom-btn btn-bg btn mt-3" data-aos-delay="100">Admin Dashboard</a><br>'
+        all_buttons += '<a href="../admindashboard/dashboard.html" class="custom-btn btn-bg btn mt-3" data-aos-delay="100">Admin Dashboard</a><br>'
     # No Roles 
     if current_user.is_owner == False and current_user.is_sitter == False and current_user.is_shelter == False and current_user.is_admin == False:
         all_buttons += '<p> Did Not Choose Any Roles. Please Modify Account.</p>'
