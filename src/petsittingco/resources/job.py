071371d22bbcb964dc828786f10af7469dede8ae
Ian Gifford <ian@thegiffordteam.org>
2020-12-08 21:06:49 -0500

    no pet id in job

--- src/petsittingco/resources/job.py
+++ src/petsittingco/resources/job.py
@@ -192,7 +192,7 @@ class JobSearch(Resource):
                 owner_name = owner.first_name
                 if not (job.accepted or job.canceled):
                     if calc_lat_long_distance(job.lat, job.long, args["lat"], args["lon"]) <= self.MAX_DISTANCE:
-                        job_dict[job.id] = {"location":job.location,"owner_name":job.pet_id, "start_datetime":job.start_datetime, "end_datetime":job.end_datetime}
+                        job_dict[job.id] = {"location":job.location, "start_datetime":job.start_datetime, "end_datetime":job.end_datetime}
             job_dict["success"] = True
             print("job_dict:",job_dict)
             return job_dict, 200 
