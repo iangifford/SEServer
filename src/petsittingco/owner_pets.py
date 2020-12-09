6cd6ef8b7d947836d9810544f68a2c4d1d8ec742
Isabelle <imrivera@loyola.edu>
2020-12-09 01:26:54 -0500

    put back Get and changed html

--- src/petsittingco/owner_pets.py
+++ src/petsittingco/owner_pets.py
@@ -6,15 +6,15 @@ from src.petsittingco.database import db, Pet
 
 pet_blueprint = Blueprint("pets","__pets__")
 
-@pet_blueprint.route('/pets', methods=['POST'])
-@pet_blueprint.route('/pets.html', methods=['POST'])
+@pet_blueprint.route('/pets', methods=['GET'])
+@pet_blueprint.route('/pets.html', methods=['GET'])
 def pets():
     pets = ""
     user_pet_array = current_user.pets
     if not user_pet_array:
-        pets += "You Have No Pets Yet. Add Some Pets!"
+        pets += '<p> You Have No Pets Yet. Add Some Pets! </p>'
 
     for pet_info in user_pet_array:
-        pets += "<br>" + pet_info.name + "<br>" + pet_info.attributes + "<br>"
+        pets += '<p> <br>' + pet_info.name + '<br>' + pet_info.attributes + '<br> </p>'
 
     return render_template("pets.html", pet_list=pets)
