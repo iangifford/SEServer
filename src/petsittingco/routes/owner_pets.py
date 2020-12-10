0122a299742d9bcedec92c3e30c6393139fe211d
Isabelle <imrivera@loyola.edu>
2020-12-10 16:13:21 -0500

    changed format of showing attributes in owner_pets.py and put min for other_info input

--- src/petsittingco/routes/owner_pets.py
+++ src/petsittingco/routes/owner_pets.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin, LoginManager, login_user, login_required, log
 from flask import Flask, send_from_directory, request, Blueprint, render_template, redirect, url_for
 from flask_restful import Resource, Api, reqparse
 from src.petsittingco.database import db, Pet
+import json 
 
 
 pet_blueprint = Blueprint("pets","__pets__")
@@ -10,12 +11,14 @@ pet_blueprint = Blueprint("pets","__pets__")
 @pet_blueprint.route('/petownerdashboard/pets.html', methods=['GET'])
 def pets():
     pets = ""
+    
     user_pet_array = current_user.pets
     if not user_pet_array:
         pets += '<p> You Have No Pets Yet. Add Some Pets! </p>'
 
     for pet_info in user_pet_array:
-        pets += '<p> <br>' + pet_info.name + '<br>' + pet_info.attributes + '<br> </p>'
+        pet_temp_dict = json.loads(pet_info.attributes)
+        pets += '<p> <br> Pet Name: ' + pet_temp_dict["pet_name"] + '<br> Pet Type: ' + pet_temp_dict["other_type"] + '<br> Attributes: ' + pet_temp_dict["energetic"] + '<br>' + pet_temp_dict["noisy"] + '<br>' + pet_temp_dict["trained"] + '<br>' + pet_temp_dict["other_info"] + '<br></p>'
 
     pets += '<br> <form action="../petownerdashboard/pet_forms.html" method="get" class="contact-form" data-aos-delay="300" role="form"> <div class="col-lg-5 mx-auto col-1"> <div class="row"> <button class="form-control" id="submit-button" >Create Pet</button> </div> </div> </form> <br>'
     return render_template("petownerdashboard/pets.html", pet_list=pets)
