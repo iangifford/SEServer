c73affa1da01a49f2d156b1852358fc6afa13375
Isabelle <imrivera@loyola.edu>
2020-12-10 18:15:00 -0500

    addded missing double quote

--- src/petsittingco/routes/owner_pets.py
+++ src/petsittingco/routes/owner_pets.py
@@ -25,7 +25,7 @@ def pets():
             pets += '<br> This Pet is Noisy <br>'
         if pet_temp_dict["trained"]:
             pets += '<br> This Pet is Trained<br>'
-        pets += '<br> Other Info: ' + pet_temp_dict["other_info"] + '<br></p> <br> <form action="../petownerdashboard/delete.html?pet_id=' + pet_info.id + ' method="get" class="contact-form" data-aos-delay="300" role="form"> <div class="col-lg-5 mx-auto col-1"> <div class="row"> <button class="form-control" id="submit-button" >Delete Pet</button> </div> </div> </form><br>'
+        pets += '<br> Other Info: ' + pet_temp_dict["other_info"] + '<br></p> <br> <form action="../petownerdashboard/delete.html?pet_id=' + pet_info.id + '" method="get" class="contact-form" data-aos-delay="300" role="form"> <div class="col-lg-5 mx-auto col-1"> <div class="row"> <button class="form-control" id="submit-button" >Delete Pet</button> </div> </div> </form><br>'
 
     pets += '<br> <form action="../petownerdashboard/pet_forms.html" method="get" class="contact-form" data-aos-delay="300" role="form"> <div class="col-lg-5 mx-auto col-1"> <div class="row"> <button class="form-control" id="submit-button" >Create Pet</button> </div> </div> </form> <br> <form action="../petownerdashboard/change_pet.html" method="get" class="contact-form" data-aos-delay="300" role="form"> <div class="col-lg-5 mx-auto col-1"> <div class="row"> <button class="form-control" id="submit-button" >Modify Pet</button> </div> </div> </form><br>'
     return render_template("petownerdashboard/pets.html", pet_list=pets)
