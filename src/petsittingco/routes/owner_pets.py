731a7f0227fe2a12aa87766b94a59cba7c821b85
Isabelle <imrivera@loyola.edu>
2020-12-10 16:33:35 -0500

    changed booleans to string messages

--- src/petsittingco/routes/owner_pets.py
+++ src/petsittingco/routes/owner_pets.py
@@ -18,7 +18,14 @@ def pets():
 
     for pet_info in user_pet_array:
         pet_temp_dict = json.loads(pet_info.attributes)
-        pets += '<p> <br> Pet Name: ' + pet_temp_dict["pet_name"] + '<br> Pet Type: ' + pet_temp_dict["other_type"] + '<br> Attributes: ' + pet_temp_dict["energetic"] + '<br>' + pet_temp_dict["noisy"] + '<br>' + pet_temp_dict["trained"] + '<br>' + pet_temp_dict["other_info"] + '<br></p>'
+        pets += '<p> <br> Pet Name: ' + pet_temp_dict["pet_name"] + '<br> Pet Type: ' + pet_temp_dict["other_type"] + '<br> Attributes: '
+        if pet_temp_dict["energetic"]:
+            pets += '<br> This Pet is Energetic <br>'
+        if pet_temp_dict["noisy"]:
+            pets += '<br> This Pet is Noisy <br>'
+        if pet_temp_dict["trained"]:
+            pets += '<br> This Pet is Trained<br>'
+        pets += '<br>' + pet_temp_dict["other_info"] + '<br></p>'
 
     pets += '<br> <form action="../petownerdashboard/pet_forms.html" method="get" class="contact-form" data-aos-delay="300" role="form"> <div class="col-lg-5 mx-auto col-1"> <div class="row"> <button class="form-control" id="submit-button" >Create Pet</button> </div> </div> </form> <br>'
     return render_template("petownerdashboard/pets.html", pet_list=pets)
