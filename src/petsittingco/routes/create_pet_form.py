6ea9a3f9cf9d4d240689c07f33cc8e0f9e505dd9
Isabelle <imrivera@loyola.edu>
2020-12-09 22:59:41 -0500

    fixed form issue and added form fields

--- src/petsittingco/routes/create_pet_form.py
+++ src/petsittingco/routes/create_pet_form.py
@@ -11,6 +11,7 @@ pet_form_blueprint = Blueprint("pet_forms","__pet_forms__")
 
 class RegisterForm(FlaskForm):
     pet_name = StringField('Pet Name', validators=[InputRequired(), Length(min=4,max=64)])
+    pet_type = StringField(' Animal ', validators=[InputRequired()] )
     is_energetic = BooleanField('Energetic?')
     is_noisy = BooleanField('Noisy?')
     is_trained = BooleanField('Trained?')
@@ -23,14 +24,14 @@ def pet_forms():
 
     if form.validate_on_submit():
         pet_attr_dict = {} 
-        pet_attributes_string = ""
-        pet_attr_dict = {"pet_name":form.pet_name, "pet_type":"", "other_type":"", "energetic":form.is_energetic,  "noisy":form.is_noisy, "trained":form.is_trained, "other info":form.other_info} 
+        # pet_attributes_string = ""
+        pet_attr_dict = {"pet_name":form.pet_name, "pet_type":"", "other_type":form.pet_type, "energetic":form.is_energetic,  "noisy":form.is_noisy, "trained":form.is_trained, "other info":form.other_info} 
         
         new_pet = Pet(id=str(uuid.uuid4()), owner_id=current_user.id, pet_name=form.pet_name, attributes=str(pet_attr_dict))
         db.session.add(new_pet)
         db.session.commit()
 
-        pet_attributes_string += "<br> <p>"+ str(pet_attr_dict) + "<br> </p>"
+        # pet_attributes_string += "<br> <p>"+ str(pet_attr_dict) + "<br> </p>"
         
         return redirect('/petownerdashboard/pets.html')
     return render_template('/petownerdashboard/pet_forms.html', pet_form=form )
